d64a7f78f47947f20c6b088211c85585349a2052
RP098 <adagor00673@gmail.com>
2018-11-08 10:19:03 +0100

    dodawanie

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+int main ()
 {
-    printf("Hello world!");
-    return 0;
+   int liczba1, liczba2, wynik;
+   char znak;
+       printf("Wpisz pierwsza liczbe : \n");
+            scanf("%d", &liczba1);
+       printf("Wpisz druga liczbe: \n");
+            scanf("%d", &liczba2);
+            getchar();
+       printf("Jaka operacje chcesz wykonac? (podaj znak - +, -, *, /): \n");
+            scanf("%c", &znak);
+
+switch (znak)
+{
+          case   '+': wynik=liczba1+liczba2;
+                   printf("Wynik to: %d\n",wynik);
+                   break;
+
+}
+return 0;
 }
